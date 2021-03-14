43d8c9941eb365288e914634047b32d1347324b1
Sabi <szigyi.szogyi@gmail.com>
2021-03-14 21:31:25 +0000

    done tasks

--- src/main/scala/hu/szigyi/ettl/web/app/WebApp.scala
+++ src/main/scala/hu/szigyi/ettl/web/app/WebApp.scala
@@ -28,8 +28,9 @@ import scala.concurrent.ExecutionContext
 // 10 make source folder of raw images a config or part of request
 // 11 make image api more robust: can serve converted image if already exist; can serve latest image after page refresh
 // TODO 12 serve the entire last log file after page is refreshed not just incoming since now -> UI should have different states when first request goes to api and then just fetching latest
-// TODO 13 raw directory path and log path are comign from command line args and UI can get them -> makes the code more flexible and independent where it is running
-// TODO 14 can handle empty raw directory
+// 13 raw directory path and log path are comign from command line args and UI can get them -> makes the code more flexible and independent where it is running
+// 14 can handle empty raw directory
+// TODO 15 read only known files types from folder
 
 // http://localhost:8230/index.html
 object WebApp extends IOApp with StrictLogging {
