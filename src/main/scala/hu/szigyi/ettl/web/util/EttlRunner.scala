c776c6010ffafe69cb871a20f4cf381b045327c3
Sabi <szigyi.szogyi@gmail.com>
2021-03-18 20:02:00 +0000

    use set settings checkbox on UI to drive ettl

--- src/main/scala/hu/szigyi/ettl/web/util/EttlRunner.scala
+++ src/main/scala/hu/szigyi/ettl/web/util/EttlRunner.scala
@@ -6,12 +6,12 @@ import com.typesafe.scalalogging.StrictLogging
 
 object EttlRunner extends StrictLogging {
 
-  def executeEttl(dummyCamera: Boolean, intervalSeconds: Int): Unit = {
+  def executeEttl(dummyCamera: Boolean, setSettings: Boolean, intervalSeconds: Int): Unit = {
     val ettl = s"""ettl
        |/Users/szabolcs/dev/expose-to-the-light/logs
-       |${if (dummyCamera) "--dummyCamera"}
+       |${if (dummyCamera) "--dummyCamera" else ""}
        |--imagesBasePath /Users/szabolcs/dev/expose-to-the-light/captured-images
-       |--setSettings
+       |${if (setSettings) "--setSettings" else ""}
        |--numberOfCaptures 5
        |--intervalSeconds $intervalSeconds
        |--rawFileExtension JPG""".stripMargin
