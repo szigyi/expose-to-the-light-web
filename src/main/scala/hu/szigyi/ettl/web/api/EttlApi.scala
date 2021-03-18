e3ac843fbcda21e25cedbcf000932b75a64e2f80
Sabi <szigyi.szogyi@gmail.com>
2021-03-18 20:16:16 +0000

    use numberOfCaptures input on UI to drive ettl

--- src/main/scala/hu/szigyi/ettl/web/api/EttlApi.scala
+++ src/main/scala/hu/szigyi/ettl/web/api/EttlApi.scala
@@ -16,7 +16,7 @@ class EttlApi extends Http4sDsl[IO] with StrictLogging {
     case request @ POST -> Root =>
       request.decode[EttlRequest] { req =>
       logger.info(s"Request: $req")
-        Ok(EttlRunner.executeEttl(req.dummyCamera, req.setSettings, req.intervalSeconds))
+        Ok(EttlRunner.executeEttl(req.dummyCamera, req.setSettings, req.numberOfCaptures, req.intervalSeconds))
       }
   }
 }
@@ -24,5 +24,5 @@ class EttlApi extends Http4sDsl[IO] with StrictLogging {
 object EttlApi {
   implicit val ettlRequestCodec: Codec[EttlRequest] = deriveCodec[EttlRequest]
 
-  case class EttlRequest(dummyCamera: Boolean, setSettings: Boolean, intervalSeconds: Int)
+  case class EttlRequest(dummyCamera: Boolean, setSettings: Boolean, numberOfCaptures: Int, intervalSeconds: Int)
 }
--- src/main/scala/hu/szigyi/ettl/web/util/EttlRunner.scala
+++ src/main/scala/hu/szigyi/ettl/web/util/EttlRunner.scala
@@ -6,13 +6,16 @@ import com.typesafe.scalalogging.StrictLogging
 
 object EttlRunner extends StrictLogging {
 
-  def executeEttl(dummyCamera: Boolean, setSettings: Boolean, intervalSeconds: Int): Unit = {
+  def executeEttl(dummyCamera: Boolean,
+                  setSettings: Boolean,
+                  numberOfCaptures: Int,
+                  intervalSeconds: Int): Unit = {
     val ettl = s"""ettl
        |/Users/szabolcs/dev/expose-to-the-light/logs
        |${if (dummyCamera) "--dummyCamera" else ""}
        |--imagesBasePath /Users/szabolcs/dev/expose-to-the-light/captured-images
        |${if (setSettings) "--setSettings" else ""}
-       |--numberOfCaptures 5
+       |--numberOfCaptures $numberOfCaptures
        |--intervalSeconds $intervalSeconds
        |--rawFileExtension JPG""".stripMargin
 
