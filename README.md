54217ef28c5b6a8e6a864b5ddaff0a744579c7e6
Sabi <szigyi.szogyi@gmail.com>
2021-03-23 23:41:18 +0000

    released version 0.1.12

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ expose-to-the-light-web client
 
 ## Install application
 ### Download installer
-`curl -L -o install-web.sh https://www.dropbox.com/s/wzmrdngxpl1dh41/install-web.sh?dl=1`
+`curl -L -o install-web.sh https://www.dropbox.com/s/t2cpsbv2lm21h5b/install-web.sh?dl=1`
 
 ### Install
 * `chmod u+x install-web.sh`
--- install-web.sh
+++ install-web.sh
@@ -9,9 +9,9 @@ replace_string() {
   sed -i '' -e "s/$placeholder/$ESCAPED_REPLACE/" "$file_name"
 }
 
-artifact_link="https://www.dropbox.com/s/bg6wl2hkgjqg2gw/expose-to-the-light-web_2.13-0.1.11.jar?dl=1"
-ettl_web_link="https://www.dropbox.com/s/ze6zrtwipzx6xee/ettl-web?dl=1"
-artifact="expose-to-the-light-web_2.13-0.1.11.jar"
+artifact_link="https://www.dropbox.com/s/wos76tb3bof87e7/expose-to-the-light-web_2.13-0.1.12.jar?dl=1"
+ettl_web_link="https://www.dropbox.com/s/qnsm904jyud58vl/ettl-web?dl=1"
+artifact="expose-to-the-light-web_2.13-0.1.12.jar"
 
 echo "Creating app folder at /usr/local/opt/ettl"
 sudo mkdir -p /usr/local/opt/ettl
