67df117e09b23a0cc4a4c857ba631f0a3e741453
Sabi <szigyi.szogyi@gmail.com>
2021-04-12 08:25:09 +0100

    more todos

--- src/main/scala/hu/szigyi/ettl/web/app/WebEttlApp.scala
+++ src/main/scala/hu/szigyi/ettl/web/app/WebEttlApp.scala
@@ -55,6 +55,9 @@ import scala.concurrent.ExecutionContext
 // 38 if ettl is not running then stop polling images, logs, metrics
 // 39 add progress bar to preview page
 // 40 add timelapse length estimator based on the # of captures and interval, like replaying it in 30fps it will be 2 secs long
+// TODO 41 add description, project's goal to README
+// TODO 42 lists the main features of the app in README
+// TODO 43 link a video that showcases the features in README
 
 object WebEttlApp extends IOApp with StrictLogging {
 
